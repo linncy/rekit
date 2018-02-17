8de991a3bed6bc51a80e7eaa2e21e4762b16970e
Lincy <linchangyuan1996@gmail.com>
2018-02-16 21:25:52 -0700

    Postman
    Test Pass

--- rekit/postman.py
+++ rekit/postman.py
@@ -1,4 +1,4 @@
-import smtplib
+import smtplib, imaplib, email
 from threading import Thread
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -6,28 +6,55 @@ from pyorgtex.convert import *
 def createconfig(JSONname):
 	newdict={}
 	newdict['MAIL_ADDRESS']='example@gmail.com'
-	newdict['MAIL_SERVER']='smtp.googlemail.com'
-	newdict['MAIL_PORT']=587
+	newdict['MAIL_SMTP_SERVER']='smtp.gmail.com'
+	newdict['MAIL_IMAP_SERVER']='imap.gmail.com'
+	newdict['MAIL_SMTP_PORT']=587
+	newdict['MAIL_IMAP_PORT']=993
 	newdict['MAIL_USE_TLS']=True
-	newdict['MAIL_USE_SSL']=False
+	newdict['MAIL_USE_SSL']=True
 	newdict['MAIL_USERNAME']='mailusername'
 	newdict['MAIL_PASSWORD']='mailpassword'
-	dict2json(newdict,mail_conf.json)
+	dict2json(newdict,JSONname)
 	print(JSONname +'created.')
-	
-def sendmail(configdict,to,subject,attachmentlist,_from='Reproducible Hw',body='This is an email sent from juno.\n Juno lives at the 13th floor, ICE \n Good Luck! '):
+
+def sendmail(configdict,to,subject,attachmentlist,_from='Juno',body='This is an email from Juno.\nJuno lives at 13th floor, ICE \nGood Luck! '):
 	msg = MIMEMultipart()
 	msg['Subject']=subject
 	msg['To']=to
 	msg['From']=_from
 	msg.preamble=body
+	msg.attach(MIMEText(body))
 	for item in attachmentlist:
 		attachment=MIMEText(open(item,'rb').read(),'base64','utf-8')
 		attachment["Content-Type"]='application/octet-stream'
 		attachment["Content-Disposition"]='attachment; filename="'+extractFilename(item)+'"'
 		msg.attach(attachment)
-	server = smtplib.SMTP(configdict['MAIL_SERVER'],configdict['MAIL_PORT'])
+	server = smtplib.SMTP(configdict['MAIL_SMTP_SERVER'],configdict['MAIL_SMTP_PORT'])
 	server.set_debuglevel(1)
-	server.login(newdict['MAIL_ADDRESS'],['MAIL_PASSWORD'])
-	server.sendmail(msg)
+	server.ehlo()
+	if(configdict['MAIL_USE_SSL']==True):
+		server.starttls()
+	server.login(configdict['MAIL_ADDRESS'],configdict['MAIL_PASSWORD'])
+	server.sendmail(_from,to,msg.as_string())
 	server.quit()
+
+def retrievemail(configdict,inbox='INBOX'):
+	if configdict['MAIL_USE_SSL']==True:
+		imapserver=imaplib.IMAP4_SSL(port=configdict['MAIL_IMAP_PORT'],host=configdict['MAIL_IMAP_SERVER'])
+	else:
+		imapserver=imaplib.IMAP4(port=configdict['MAIL_IMAP_PORT'],host=configdict['MAIL_IMAP_SERVER'])
+	imapserver.login(configdict['MAIL_ADDRESS'],configdict['MAIL_PASSWORD'])
+	imapserver.select(inbox)
+	typ, data = imapserver.search(None, 'unseen') #Fetch unread mail only
+	msglist=[]
+	for num in data[0].split():
+		typ, data = imapserver.fetch(num, '(RFC822)')
+		imapserver.store(num,'+FLAGS','\\Seen') #Marked mail as seen
+		msg = email.message_from_string(data[0][1].decode('utf-8')) 
+		msglist.append(msg)
+	imapserver.close()
+	imapserver.logout()
+	return msglist
+
+def mailchecker(msg,criteriondict): #Maybe define it elsewhere
+	subjectlist=convert.extract_data_from_curly_brackets(msg['subject'])
